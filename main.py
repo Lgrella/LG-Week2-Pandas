14cefef9dd5199208266874251d007e764cbefa4
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-09-17 18:51:37 +0000

    update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 Created on Fri Sep 15 15:03:11 2023
 @author: lillygrella
 """
-
+#from datetime import datetime
 import pandas as pd
 #import polars as pl
 from matplotlib import pyplot as plt, dates
 from matplotlib.ticker import FuncFormatter
-from datetime import datetime
 
-#read in stock data based on ticker list and dates, restrict to days when the market is open based on SPY
+#read in stock data based on ticker list and dates, restrict to when the market is open based on SPY
 
 def readin(ticker):
     '''
@@ -28,41 +27,35 @@ def get_summ_stats():
     stats_summary = d_f['PriceChange'].describe()
     return stats_summary
 
-def dollars(x):
+def dollars(value):
     '''
     Format floats to dollars
     'The two args are the value and tick position'
     '''
-    return '$%1.0f' % (x)
+    return f'${value:.0f}'
 
 
 def make_line_graph():
     '''
     Create Line Graph of 
     '''
-
-    df = readin('SPY')
-    
+    d_f = readin('SPY')  
     # Create a new figure and a subplot
     #fig, ax = plt.subplots()
     # Plot the OHLC data
     #df['Close'].plot()
     # Set the title and labels
     #ax.xaxis.set_major_formatter(dates.DateFormatter('%m-%y'))
-   
-    
-    ax = plt.subplots()
-    df['Close'].plot()
-    ax.set_xticks(df.index)
+    figure,a_x = plt.subplots()
+    d_f['Close'].plot()
+    a_x.set_xticks(d_f.index)
     plt.locator_params(axis='x', nbins=12)
-    ax.set_title('SPY Closing Stock Price')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Closing Price')
-    
+    a_x.set_title('SPY Closing Stock Price')
+    a_x.set_xlabel('Date')
+    a_x.set_ylabel('Closing Price')   
     # use formatters to specify major and minor ticks
-    ax.xaxis.set_major_formatter(dates.DateFormatter("%m/%y"))
-    ax.yaxis.set_major_formatter(FuncFormatter(dollars))
-    #_ = plt.xticks(rotation=90)    
+    a_x.xaxis.set_major_formatter(dates.DateFormatter("%m/%y"))
+    a_x.yaxis.set_major_formatter(FuncFormatter(dollars))
+    #_ = plt.xticks(rotation=90)  
     # Show the plot
     plt.savefig("SPY_Closing.png")
-    
