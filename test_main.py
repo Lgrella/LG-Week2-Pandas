617ab29e26f21962b5b97330628dc6b49f1650bd
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-09-18 20:31:47 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 # test test_main.py
-from main import readin, get_summ_stats
+from main import readin, get_summ_stats,make_line_graph
 
 def test_sumstats():
     """
@@ -13,6 +13,14 @@ def test_sumstats():
     assert spy["Close"].median().round(2) == series.iloc[5].round(2)
     assert spy["Close"].std().round(2) == series.iloc[2].round(2)
 
+def test_plot():
+    """
+    Test defined seeplot function to checkout a scatter plot
+    """
+    spy = readin()
+    return make_line_graph(spy)
+
 if __name__ == "__main__":
     test_sumstats()
+    test_plot()
 
